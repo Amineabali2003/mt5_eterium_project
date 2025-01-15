4459ca4451986c0f289ad4daceff9e2825dde59b
ABALI Mohammed Amine <abalimohammedamine@gmail.com>
2025-01-15 14:55:50 +0100

    Add Profile update wallet

--- server/internal/controllers/users.go
+++ server/internal/controllers/users.go
@@ -10,18 +10,14 @@ import (
 
 func (r controller) createUser(c echo.Context) error {
 	req := model.CreateUserReqesut{}
-
 	if err := c.Bind(&req); err != nil {
 		return err
 	}
-
 	user, err := r.service.CreateUser(req)
 	if err != nil {
 		return err
 	}
-
 	return c.JSON(http.StatusCreated, user)
-
 }
 
 func (r controller) getCurrentUser(c echo.Context) error {
@@ -29,11 +25,38 @@ func (r controller) getCurrentUser(c echo.Context) error {
 	if err != nil {
 		return echo.NewHTTPError(http.StatusUnauthorized, err)
 	}
-
 	res := model.User{
 		ID:    user.ID,
 		Email: user.Email,
 	}
-
 	return c.JSON(http.StatusOK, res)
+}
+
+func (r controller) getWallet(c echo.Context) error {
+	user, err := middlewares.GetUser(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
+	}
+	if user.WalletAddress == "" {
+		return c.JSON(http.StatusOK, map[string]string{})
+	}
+	return c.JSON(http.StatusOK, map[string]string{"wallet": user.WalletAddress})
+}
+
+func (r controller) updateWallet(c echo.Context) error {
+	user, err := middlewares.GetUser(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
+	}
+	var body struct {
+		Wallet string `json:"wallet" validate:"required"`
+	}
+	if err := c.Bind(&body); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid request")
+	}
+	updatedUser, err := r.service.UpdateWallet(user.ID, body.Wallet)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to update wallet")
+	}
+	return c.JSON(http.StatusOK, updatedUser)
 }
--- server/internal/repositories/users.go
+++ server/internal/repositories/users.go
@@ -8,33 +8,24 @@ import (
 )
 
 func (r repository) CreateUser(user model.User) (model.User, error) {
-
 	user.CreatedAt = time.Now().UTC()
 	user.UpdatedAt = time.Now().UTC()
-
 	_, err := r.db.NewInsert().Model(&user).ExcludeColumn("id").Returning("*").Exec(context.TODO())
 	if err != nil {
 		return model.User{}, err
 	}
-
 	return user, nil
 }
 
 func (r repository) GetUserByEmail(email string) (model.User, error) {
-
 	user := model.User{}
-
 	err := r.db.NewSelect().Model(&user).Where("email = ?", email).Scan(context.TODO())
-
 	return user, err
 }
 
 func (r repository) GetUser(id string) (model.User, error) {
-
 	user := model.User{}
-
 	err := r.db.NewSelect().Model(&user).Where("id = ?", id).Scan(context.TODO())
-
 	return user, err
 }
 
@@ -42,18 +33,16 @@ func (r repository) UpdateUser(id string, user model.User) (model.User, error) {
 	updateUser := map[string]interface{}{
 		"updated_at": time.Now().UTC(),
 	}
-
 	query := r.db.NewUpdate().Model(&updateUser).TableExpr("users")
-
 	if user.Password != "" {
 		updateUser["password"] = user.Password
 	}
-
 	if user.IsEmailVerified {
 		updateUser["is_email_verified"] = user.IsEmailVerified
 	}
+	if user.WalletAddress != "" {
+		updateUser["wallet_address"] = user.WalletAddress
+	}
 	_, err := query.Where("id = ?", id).Returning("*").Exec(context.TODO())
-
 	return user, err
-
 }
